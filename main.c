b00f562796ecbbc1551f0e55609ef396391ff934
Samvel Abrahamyan <samvel1024@gmail.com>
2017-11-08 12:33:19 +0100

    Solved in linear

--- main.c
+++ main.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("First breakpoint passsed");
-    while(1){
-        ++i;
+int main()
+{
+
+  
+  int n;
+  scanf("%d", &n);
+  int *incr = (int*)malloc(sizeof(int) * n);
+  int *decr = (int*)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; ++i)
+  {
+    scanf("%d", &incr[i]);
+    scanf("%d", &decr[i]);
+  }
+  int pos = -1;
+  for (int i = 0; i < n; ++i)
+  {
+    if (incr[i] >= decr[i])
+    {
+      pos = i;
+      break;
     }
-    printf("Qwerty %d", i);
-    printf("\n");
+  }
+
+  if (incr[pos] == -1)
+  {
+    printf("false");
     return 0;
+  }
+  int incrSum = incr[pos];
+  int decrSum = decr[pos];
+  if (incrSum == decrSum)
+    printf("true");
+  else if (pos >= 1 && incrSum + incr[pos - 1] == decrSum + decr[pos - 1])
+    printf("true");
+  else printf("false");
+
+  return 0;
 }

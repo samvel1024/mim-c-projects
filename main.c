21cc4ef926ba87895f65581ad4e58788fbb6e7a4
Samvel Abrahamyan <samvel1024@gmail.com>
2017-11-14 23:52:45 +0100

    Linear dynamic solution

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("Input number is %d", i);
-    printf("\n");
+#define SEQUENCE_LEN 3
+
+int main()
+{
+    int n;
+    //m[a][b] shows the count of sequences of length a before index b
+    int m[SEQUENCE_LEN][100000];
+    int a[100000];
+
+    scanf("%d", &n);
+    int zeroCount = 0;
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &a[i]);
+        m[0][i] = (a[i] == 0) ? ++zeroCount : zeroCount;
+    }
+
+    for (int len = 1; len < SEQUENCE_LEN; ++len) {
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            count = (a[i] == len) ? count + m[len - 1][i] : count;
+            m[len][i] = count;
+        }
+    }
+
+    printf("%d", m[SEQUENCE_LEN - 1][n - 1]);
+
     return 0;
 }

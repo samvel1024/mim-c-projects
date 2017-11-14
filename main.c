719c812fb9213ab717050256cc339dc18651c9c4
Samvel Abrahamyan <samvel1024@gmail.com>
2017-11-14 23:26:35 +0100

    Change template

--- main.c
+++ main.c
@@ -3,11 +3,7 @@
 int main(){
     int i;
     scanf("%d", &i);
-    printf("First breakpoint passsed");
-    while(1){
-        ++i;
-    }
-    printf("Qwerty %d", i);
+    printf("Input number is %d", i);
     printf("\n");
     return 0;
 }

b8d75ca24271dcb269371c560a993a2959559fdf
Samvel Abrahamyan <samvel1024@gmail.com>
2017-12-09 22:06:34 +0100

    Refactor

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ void fake_printf(const char *format, ...) {
 }
 
 typedef enum {
-  WRITE = 100,
+  WRITE,
   READ,
   SUBTRACT,
   CALL_LABEL,
@@ -45,7 +45,7 @@ typedef struct command_t {
 
 
 void Command_log(Command *this) {
-  const char *name = COMMAND_NAMES[this->type - WRITE];
+  const char *name = COMMAND_NAMES[this->type];
   log("%s(%s%s%s) %s %s\n", name,
       this->arg1, strlen(this->arg2) == 0 ? "" : ",",
       this->arg2,
@@ -84,9 +84,9 @@ void Parser_skip(Parser *self) {
   while (self->has_next && Parser_is_whitespace(self->buff) && Parser_read(self));
 }
 
-Parser* Parser_new(){
+Parser *Parser_new() {
   Parser *self = malloc(sizeof(Parser));
-  self -> has_next = true;
+  self->has_next = true;
   Parser_read(self);
   Parser_skip(self);
   return self;
@@ -112,13 +112,12 @@ void Parser_read_label(Parser *self, Command *c) {
 }
 
 Command Parser_next_command(Parser *self) {
-  Command c = {.is_labeled = false, .arg1 = "", .arg2 = "", .type = 0, .label = ""};
+  Command c = {.is_labeled = false, .arg1 = "", .arg2 = "", .type = -1, .label = ""};
   Parser_read_label(self, &c);
   if (self->buff == ';') {
     c.type = RETURN;
     Parser_read(self);
-  }
-  else if (self->buff == '^') {
+  } else if (self->buff == '^') {
     Parser_read(self);
     Parser_skip(self);
     Parser_read_word(self, c.arg1, Parser_is_number);
@@ -126,18 +125,17 @@ Command Parser_next_command(Parser *self) {
   } else if (Parser_is_number(self->buff)) {
     Parser_read_word(self, c.arg1, Parser_is_number);
     Parser_skip(self);
-    if (self -> buff == '^'){
+    if (self->buff == '^') {
       Parser_read(self);
       c.type = WRITE;
-    }else if (Parser_is_number(self -> buff)){
+    } else if (Parser_is_number(self->buff)) {
       Parser_read_word(self, c.arg2, Parser_is_number);
       c.type = SUBTRACT;
-    }
-    else {
+    } else {
       Parser_read_word(self, c.arg2, Parser_not_whitespace);
       c.type = CONDITION;
     }
-  }else {
+  } else {
     Parser_read_word(self, c.arg1, Parser_not_whitespace);
     c.type = CALL_LABEL;
   }
@@ -150,7 +148,7 @@ Command Parser_next_command(Parser *self) {
 typedef struct vm_t {
   Command program[PROGRAM_LEN_MAX];
   int program_len;
-  int memory[2 * ADDR_MAX];
+  int heap[2 * ADDR_MAX];
   int stack[STACK_MAX];
   int stack_size;
 } VM;
@@ -160,26 +158,26 @@ VM *VM_new() {
   vm->program_len = 0;
   vm->stack_size = 0;
   for (int i = -ADDR_MAX; i < ADDR_MAX; ++i) {
-    vm->memory[i + ADDR_MAX] = -1 - i;
+    vm->heap[i + ADDR_MAX] = -1 - i;
   }
   return vm;
 }
 
-void VM_write(VM *this, int addr, int val) {
-  this->memory[addr + ADDR_MAX] = val;
-  log("VM: Write addr %d val %d\n", addr, this->memory[addr + ADDR_MAX]);
+void VM_write(VM *self, int addr, int val) {
+  self->heap[addr + ADDR_MAX] = val;
+  log("VM: Write addr %d val %d\n", addr, self->heap[addr + ADDR_MAX]);
 }
 
-int VM_read(VM *this, int addr) {
+int VM_read(VM *self, int addr) {
   if (addr < -ADDR_MAX || addr > ADDR_MAX)
     return -1 - addr;
-  log("VM: Read  addr %d val %d\n", addr, this->memory[addr + ADDR_MAX]);
-  return this->memory[addr + ADDR_MAX];
+  log("VM: Read  addr %d val %d\n", addr, self->heap[addr + ADDR_MAX]);
+  return self->heap[addr + ADDR_MAX];
 }
 
-int VM_find_labeled(VM *this, char *label) {
-  for (int i = 0; i < this->program_len; ++i) {
-    if (this -> program[i].is_labeled && strcmp(label, this->program[i].label) == 0) {
+int VM_find_labeled(VM *self, char *label) {
+  for (int i = 0; i < self->program_len; ++i) {
+    if (self->program[i].is_labeled && strcmp(label, self->program[i].label) == 0) {
       return i;
     }
   }
@@ -212,62 +210,65 @@ void VM_increment_peek(VM *self) {
   (self->stack[self->stack_size - 1])++;
 }
 
-void VM_print_stdout(int val) {
+
+void VM_exec_write(VM *self, Command *comm) {
+  int arg1 = atoi(comm->arg1);
+  int val = VM_read(self, VM_read(self, arg1));
   if (DECORATE_STDOUT)
     log("VM_STDOUT: %c\n", (char) val);
   else
     putchar((char) val);
+  VM_increment_peek(self);
 }
+
+void VM_exec_read(VM *self, Command *comm) {
+  int arg1 = atoi(comm->arg1);
+  int in = getchar();
+  int addr = VM_read(self, arg1);
+  VM_write(self, addr, in == EOF ? -1 : in);
+  VM_increment_peek(self);
+}
+
+void VM_exec_subtract(VM *self, Command *comm) {
+  int arg1 = atoi(comm->arg1);
+  int arg2 = atoi(comm->arg2);
+  int val1 = VM_read(self, VM_read(self, arg1));
+  int val2 = VM_read(self, VM_read(self, arg2));
+  VM_write(self, VM_read(self, arg1), val1 - val2);
+  VM_increment_peek(self);
+}
+
+void VM_exec_call_label(VM *self, Command *comm) {
+  log("VM_CALL: %s\n", comm->arg1);
+  VM_increment_peek(self);
+  VM_push_stack(self, VM_find_labeled(self, comm->arg1));
+}
+
+void VM_exec_condition(VM *self, Command *comm) {
+  int arg1 = atoi(comm->arg1);
+  if (VM_read(self, VM_read(self, arg1)) > 0) {
+    log("VM_CONDITION_TRUE %s\n", comm->arg1);
+    VM_set_peek(self, VM_find_labeled(self, comm->arg2));
+  } else {
+    log("VM_CONDITION_WRONG\n");
+    VM_increment_peek(self);
+  }
+}
+
+void VM_exec_return(VM *self, Command *comm) {
+  UNUSED(comm);
+  VM_pop_stack(self);
+}
+
+void (*VM_COMMAND_EXECUTORS[])(VM*, Command*) = {VM_exec_write, VM_exec_read, VM_exec_subtract, VM_exec_call_label, VM_exec_condition, VM_exec_return};
 
 void VM_run_stack_peek(VM *self) {
   int comm_index = VM_peek_stack(self);
   Command *comm = &(self->program[comm_index]);
-  int arg1, arg2;
   log("_________NEXT_COMMAND_____________\n");
   log("VM: Stack Size %d , Current Command index %d\n", self->stack_size, comm_index);
   Command_log(comm);
-
-  switch (comm->type) {
-    case WRITE:
-      arg1 = atoi(comm->arg1);
-      VM_print_stdout(VM_read(self, VM_read(self, arg1)));
-      VM_increment_peek(self);
-      break;
-    case READ:
-      arg1 = atoi(comm->arg1);
-      int in = getchar();
-      int addr = VM_read(self, arg1);
-      VM_write(self, addr, in == EOF ? -1 : in);
-      VM_increment_peek(self);
-      break;
-    case SUBTRACT:
-      arg1 = atoi(comm->arg1);
-      arg2 = atoi(comm->arg2);
-      int val1 = VM_read(self, VM_read(self, arg1));
-      int val2 = VM_read(self, VM_read(self, arg2));
-      VM_write(self, VM_read(self, arg1), val1 - val2);
-      VM_increment_peek(self);
-      break;
-    case CALL_LABEL:
-      log("VM_CALL: %s\n", comm->arg1);
-      VM_increment_peek(self);
-      VM_push_stack(self, VM_find_labeled(self, comm->arg1));
-      break;
-    case CONDITION:
-      arg1 = atoi(comm->arg1);
-      if (VM_read(self, VM_read(self, arg1)) > 0) {
-        log("VM_CONDITION_TRUE %s\n", comm->arg1);
-        VM_set_peek(self, VM_find_labeled(self, comm->arg2));
-      } else {
-        log("VM_CONDITION_WRONG\n");
-        VM_increment_peek(self);
-      }
-      break;
-    case RETURN:
-      VM_pop_stack(self);
-      break;
-  }
-
+  VM_COMMAND_EXECUTORS[comm->type](self, comm);
 }
 
 
@@ -284,11 +285,11 @@ void VM_run_program(VM *self) {
 void VM_read_program(VM *self) {
   log("*** PARSING SOURCE CODE ***\n");
   Parser *parser = Parser_new();
-  while (parser -> has_next) {
+  while (parser->has_next) {
     Command command = Parser_next_command(parser);
-    log("%d\t", self -> program_len);
+    log("%d\t", self->program_len);
     Command_log(&command);
-    self->program[self -> program_len ++] = command;
+    self->program[self->program_len++] = command;
   }
   free(parser);
 }

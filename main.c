51a8fae53427c8977b2b1bd67b6b1411ca8af747
Samvel Abrahamyan <samvel1024@gmail.com>
2017-12-13 14:44:25 +0100

    Struct

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list_t{
+  int val;
+  struct list_t *next;
+}List;
+
+List* List_new(int val, List *next){
+  List *l = malloc(sizeof(List));
+  l -> val = val;
+  l -> next = next;
+  return l;
+}
+
+List* List_new_stdin(){
+  int size;
+  scanf("%d", &size);
+  List *l = NULL;
+  while(size--){
+    int val;
+    scanf("%d", &val);
+    l = List_new(val, NULL);
+
+
+  }
+  return l;
+}
+
 
 int main(){
-    int i;
-    scanf("%d", &i);
-    printf("%d\n", 2*i);
-    return 0;
+
 }

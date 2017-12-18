ed19efeb9ea5114cbf2859682c2971af36de7e70
Samvel Abrahamyan <samvel1024@gmail.com>
2017-12-19 00:55:48 +0100

    done

--- main.c
+++ main.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("%d\n", 2*i);
-    return 0;
+
+int N = 3;
+bool used[100];
+int perm[100];
+
+
+void f(int n, bool used[], int perm[], int used_size) {
+
+  if (used_size == n) {
+    for (int i = 0; i < n; ++i)
+      printf("%d ", perm[i]);
+    printf("\n");
+    return;
+  }
+
+  for (int i = 1; i <= n; ++i) {
+    if (!used[i - 1]) {
+      perm[used_size] = i;
+      used[i - 1] = true;
+      f(n, used, perm, used_size + 1);
+      used[i - 1] = false;
+    }
+  }
+
+}
+
+
+void swap(int *x, int *y) {
+  char temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+
+void permute(int *a, int l, int r) {
+
+  if (l == r) {
+    for (int i = 0; i < N; ++i)
+      printf("%d ", a[i]);
+    printf("\n");
+  }
+  for (int i = l; i <= r; i++) {
+    swap((a + l), (a + i));
+    permute(a, l + 1, r);
+    swap((a + l), (a + i)); //backtrack
+  }
+
+}
+
+
+int main() {
+//  f(N, used, perm, 0);
+  int nums[] = {1, 2, 3};
+  permute(nums, 0, 2);
+  return 0;
 }

b04c19b9eb86dcb3e047ad064fa6f155bff534b8
Samvel Abrahamyan <samvel1024@gmail.com>
2017-12-19 18:08:15 +0100

    refactor

--- main.c
+++ main.c
@@ -1,52 +1,25 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-
-int N = 3;
-bool used[100];
-int perm[100];
-
-
-void f(int n, bool used[], int perm[], int used_size) {
-
-  if (used_size == n) {
-    for (int i = 0; i < n; ++i)
-      printf("%d ", perm[i]);
-    printf("\n");
-    return;
-  }
-
-  for (int i = 1; i <= n; ++i) {
-    if (!used[i - 1]) {
-      perm[used_size] = i;
-      used[i - 1] = true;
-      f(n, used, perm, used_size + 1);
-      used[i - 1] = false;
-    }
-  }
-
-}
 
 
 void swap(int *x, int *y) {
-  char temp;
+  int temp;
   temp = *x;
   *x = *y;
   *y = temp;
 }
 
 
-void permute(int *a, int l, int r) {
+void permute(int perm[], int left, int right, int N) {
 
-  if (l == r) {
+  if (left == right) {
     for (int i = 0; i < N; ++i)
-      printf("%d ", a[i]);
+      printf("%d ", perm[i]);
     printf("\n");
   }
-  for (int i = l; i <= r; i++) {
-    swap((a + l), (a + i));
-    permute(a, l + 1, r);
-    swap((a + l), (a + i)); //backtrack
+  for (int i = left; i <= right; i++) {
+    swap((perm + left), (perm + i));
+    permute(perm, left + 1, right, N);
+    swap((perm + left), (perm + i)); //backtrack
   }
 
 }
@@ -54,7 +27,9 @@ void permute(int *a, int l, int r) {
 
 int main() {
 //  f(N, used, perm, 0);
-  int nums[] = {1, 2, 3};
-  permute(nums, 0, 2);
+  int N = 3;
+  int nums[N];
+  for (int i=0; i<N ; ++i) nums[i] = i+1;
+  permute(nums, 0, N-1, N);
   return 0;
 }

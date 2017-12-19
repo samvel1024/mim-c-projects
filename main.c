8f5a12271b14811a3efec99cd650dfdbe7fb0a91
Samvel Abrahamyan <samvel1024@gmail.com>
2017-12-19 18:03:36 +0100

    DOne'

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("%d\n", 2*i);
-    return 0;
+
+void f(int subset[], int subset_size, int super_set_size, int curr_element) {
+
+  if (curr_element == super_set_size) {
+    printf("{");
+    for (int i =0; i< subset_size ; ++i){
+      printf("%d ", subset[i]);
+    }
+    printf("}\n");
+    return;
+  }
+
+
+  f(subset, subset_size, super_set_size, curr_element + 1);
+  subset[subset_size] = curr_element + 1;
+  f(subset, subset_size + 1, super_set_size, curr_element + 1);
+
+
+}
+
+
+int main() {
+  int N;
+  scanf("%d", &N);
+  int subset[N + 1];
+  f(subset, 0, N, 0);
 }

6c7d09ea33783fa6b5bead5c1ed8cfd6c413e356
Samvel Abrahamyan <samvel1024@gmail.com>
2017-12-19 16:29:07 +0100

    TODO

--- main.c
+++ main.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("%d\n", 2*i);
-    return 0;
+
+int count(int n, int summand) {
+  if (summand == 1) return 1;
+  int ways = 0;
+  for (int i = 1; i <= n - summand + 1; ++i)
+    ways += count(n - i, summand - 1);
+  return ways;
+}
+
+
+int main() {
+  int N;
+  int K;
+  scanf("%d %d", &N, &K);
+  printf("%d", count(N, K));
+  return 0;
+}
+
+#include<stdio.h>
+
+void printarray(int p[], int n) {
+  int i;
+  for (i = 0; i < n; i++)
+    printf("%d ", p[i]);
+  printf("\n");
+}
+
+void partition(int n) {
+  int p[n]; // An array to store a partition
+  int k = 0;  // Index of last element in a partition
+  p[k] = n;  // Initialize first partition as number itself
+  int rem_val;
+  // This loop first prints current partition, then generates next
+  // partition. The loop stops when the current partition has all 1s
+  while (1) {
+    // print current partition
+    printarray(p, k + 1);
+    rem_val = 0;
+    while (k >= 0 && p[k] == 1) {
+      rem_val += p[k];
+      k--;
+    }
+
+    // if k < 0, all the values are 1 so there are no more partitions
+    if (k < 0) return;
+
+    // Decrease the p[k] found above and adjust the rem_val
+    p[k]--;
+    rem_val++;
+
+
+    // If rem_val is more, then the sorted order is violated.  Divide
+    // rem_val in different values of size p[k] and copy these values at
+    // different positions after p[k]
+    while (rem_val > p[k]) {
+      p[k + 1] = p[k];
+      rem_val = rem_val - p[k];
+      k++;
+    }
+
+    // Copy rem_val to next position and increment position
+    p[k + 1] = rem_val;
+    k++;
+  }
+}
+
+int main() {
+  int num;
+  printf("\nEnter a number to perform integer partition: ");
+  scanf("%d", &num);
+  partition(num);
+  return 0;
 }

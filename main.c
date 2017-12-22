7ad5785b5ba1bc36bf72300fe03ae2cab06ae0da
Samvel Abrahamyan <samvel1024@gmail.com>
2017-12-22 06:30:19 +0100

    SOlved

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("%d\n", 2*i);
-    return 0;
+
+bool f(int set[], int set_size, int sums[], int partitions, int curr_index) {
+  if (curr_index == set_size) {
+    for (int i = 1; i < partitions; ++i) {
+      if (sums[i] != sums[i - 1])
+        return false;
+    }
+    return true;
+  }
+
+  for (int i = 0; i < partitions; ++i) {
+    sums[i] += set[curr_index];
+    if ((f(set, set_size, sums, partitions, curr_index + 1)))
+      return true;
+    sums[i] -= set[curr_index];
+  }
+
+  return false;
+}
+
+int main() {
+
+
+  int partitions;
+  int set_size;
+  scanf("%d", &set_size);
+  scanf("%d", &partitions);
+  int set[set_size];
+  int *sums = calloc(partitions, sizeof(int));
+  for (int i = 0; i < set_size; ++i)
+    scanf("%d", &set[i]);
+  bool possible = f(set, 5, sums, partitions, 0);
+  printf("%d", possible);
+  return 0;
 }

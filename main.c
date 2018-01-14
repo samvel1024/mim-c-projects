7638651fd5878755d0936c411c3dd4b158165b8c
Samvel Abrahamyan <samvel1024@gmail.com>
2018-01-15 00:27:34 +0100

    Done

--- main.c
+++ main.c
@@ -38,8 +38,65 @@ Node *LL_read() {
   return head;
 }
 
+
+void solve(Node **nodes, int len) {
+
+
+
+  for (int i = 0; i < len; ++i) {
+    Node *curr = nodes[i];
+    while (curr->next && curr->next != curr) {
+      curr = curr->next;
+    }
+
+    if (curr->next == curr) {
+      curr->val++;
+    } else {
+      curr->next = Node_new(1, NULL);
+      curr->next->next = curr->next;
+    }
+
+  }
+
+  for (int i = 0; i < len; ++i) {
+    Node *curr = nodes[i];
+    while(curr -> next != curr){
+      curr = curr -> next;
+    }
+    printf("%d ", curr -> val);
+  }
+
+
+
+}
+
 int main() {
-  Node *head = LL_read();
-  LL_print(head);
+  Node *l1 = Node_new(1, NULL);
+  Node *l2 = Node_new(1, NULL);
+  Node *l3 = Node_new(1, NULL);
+  Node *l4 = Node_new(1, NULL);
+  Node *l5 = Node_new(1, NULL);
+
+  Node *h3 = Node_new(1, l1);
+  Node *h5 = Node_new(1, l4);
+  Node *h1 = Node_new(1, h3);
+  Node *h2 = Node_new(1, l2);
+  Node *h4 = Node_new(1, h5);
+
+
+  Node *r1 = Node_new(1, h1);
+  Node *r2 = Node_new(1, h2);
+  Node *r3 = Node_new(1, l2);
+  Node *r4 = Node_new(1, h3);
+  Node *r5 = Node_new(1, h2);
+  Node *r7 = Node_new(1, l3);
+  Node *r6 = Node_new(1, r7);
+  Node *r8 = Node_new(1, h4);
+  Node *r9 = Node_new(1, h4);
+  Node *r10 = Node_new(1, l5);
+
+  Node *nodes[] = {r1, r2, r3, r4, r5, r6, r7, r8, r9, r10};
+  solve(nodes, 10);
+
   return 0;
 }

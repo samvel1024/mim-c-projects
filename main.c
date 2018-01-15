cfc9a406c55f06f3f3ceb805b023eeb6c71a7507
Samvel Abrahamyan <samvel1024@gmail.com>
2018-01-15 11:26:00 +0100

    1

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("%d\n", 2*i);
-    return 0;
+
+typedef struct b_node {
+  int val;
+  struct b_node *left;
+  struct b_node *right;
+} Vert;
+
+
+Vert *Vert_new(int val, Vert *left, Vert *right) {
+  Vert *v = malloc(sizeof(Vert));
+  v -> val = val;
+  v -> left = left;
+  v -> right = right;
+  return v;
+}
+
+int main() {
+  int i;
+  scanf("%d", &i);
+  printf("%d\n", 2 * i);
+  return 0;
 }

a926bd6c8748f70a52175cf2224b9ce24135a4c4
Samvel Abrahamyan <samvel1024@gmail.com>
2018-01-15 21:41:29 +0100

    1

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <tkDecls.h>
-
+#include <stdlib.h>
+#include <limits.h>
 
 typedef struct Node_t {
   struct Node_t *left;
@@ -10,45 +10,34 @@ typedef struct Node_t {
 } Node;
 
 
-bool is_bst(Node *root, int *min, int *max) {
+Node *NN(int val, Node *left, Node *right){
+  Node *n = malloc(sizeof(Node));
+  n -> val = val;
+  n -> left = left;
+  n -> right = right;
+  return n;
+}
 
-  if (root->right == NULL && root->left == NULL) {
-    *min = root->val;
-    *max = root->val;
-    return true;
-  }
+Node *LN(int val){
+  return NN(val, NULL, NULL);
+}
 
-  int lmax, lmin, rmax, rmin;
-
-  bool left_bst = root->left == NULL || is_bst(root->left, &lmin, &lmax);
-  bool right_bst = root->right == NULL || is_bst(root->right, &rmin, &rmax);
-
-  if (!left_bst || !right_bst) return false;
-
-  if (root->left != NULL) {
-    if (root->val < lmax) {
-      return false;
-    }
-    *min = (*min < lmin) ? *min : lmin;
-    *max = (*max > lmax) ? *max : lmax;
-  }
-
-  if (root->right != NULL) {
-    if (root->val > rmin) {
-      return false;
-    }
-    *min = (*min < rmin) ? *min : rmin;
-    *max = (*max > rmax) ? *max : rmax;
-  }
-
-  return true;
-
+bool is_bst(Node *root, int from, int to) {
+  return (root == NULL) ? true :
+         root->val >= from && root->val <= to &&
+         is_bst(root->left, from, root->val) &&
+         is_bst(root->right, root->val, to);
 }
 
 
 int main() {
-  int i;
-  scanf("%d", &i);
-  printf("%d\n", 2 * i);
+  Node *root = NN(5,
+                  NN(3,
+                     LN(2),
+                     LN(6)),
+                  LN(6));
+  printf("%d", is_bst(root, INT_MIN, INT_MAX));
   return 0;
 }
+
+

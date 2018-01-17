6f9263c455e99c8f04b4db7d7236bc777ad8856a
Samvel Abrahamyan <samvel1024@gmail.com>
2018-01-17 20:07:15 +0100

    1

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 typedef struct node_t {
@@ -38,8 +39,37 @@ Node *LL_read() {
   return head;
 }
 
+bool is_cyclic(Node *head) {
+
+  Node *slow, *fast;
+  slow = fast = head;
+
+
+  while (slow && fast) {
+    slow = slow->next;
+    fast = (fast->next) ? fast->next->next : NULL;
+
+    if (slow == fast && slow != NULL)
+      return true;
+    if (slow == NULL || fast == NULL)
+      return false;
+  }
+
+  return false;
+
+}
+
+
 int main() {
-  Node *head = LL_read();
-  LL_print(head);
+  Node *flat = Node_new(1, Node_new(2, Node_new(3, Node_new(4, NULL))));
+
+  Node *cycle_head = Node_new(4, NULL);
+  Node *cyclic = Node_new(1, Node_new(2, Node_new(3, cycle_head)));
+  Node *cycle = Node_new(5, Node_new(6, Node_new(7, Node_new(8, cycle_head))));
+  cycle_head -> next = cycle;
+
+  printf("%d %d", is_cyclic(flat), is_cyclic(cyclic));
+
+
   return 0;
 }

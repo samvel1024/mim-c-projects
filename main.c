130c4f632eeeda484541cb3a17e2095a844392bb
Samvel Abrahamyan <samvel1024@gmail.com>
2018-01-30 19:16:47 +0100

    1

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(){
-    int i;
-    scanf("%d", &i);
-    printf("%d\n", 2*i);
-    return 0;
+
+
+int cache[10000];
+
+int f(char str[], int from) {
+  if (from == 0)
+    return 1;
+
+  bool two = from > 1 ? (str[from-1]-'0' + (str[from - 2]-'0') * 10 <= 26) ? true : false : false;
+
+  return f(str, from - 1) + (two ? f(str, from - 2) : 0);
+
+}
+
+
+int main() {
+  char str[] = "1111111111";
+  printf("%d", f(str, 10));
+  return 0;
 }

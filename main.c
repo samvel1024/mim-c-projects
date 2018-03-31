11ef2361bd1e846ae36a5f771e69a28c17d98bef
Samvel Abrahamyan <samvel1024@gmail.com>
2018-03-31 13:19:30 +0200

    Added first impl of parsing

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "tree.h"
 
 
 #define BUF_SIZE 100
@@ -16,18 +17,47 @@ typedef enum {
 	QUERY
 } CommandType;
 
-const char *COMM_ADD_USER = "addUser";
-const char *COMM_DEL_USER = "delUser";
-const char *COMM_ADD_ITEM = "addMovie";
-const char *COMM_REMOVE_ITEM = "delMovie";
-const char *COMM_QUERY = "marathon";
-
+#define COMM_ADD_NODE  "addUser"
+#define COMM_DEL_USER  "delUser"
+#define COMM_ADD_ITEM  "addMovie"
+#define COMM_DEL_ITEM  "delMovie"
+#define COMM_QUERY  "marathon"
+#define COMM_SIZE 5
+const char *COMMAND_NAMES[] = {COMM_ADD_ITEM, COMM_ADD_NODE, COMM_DEL_USER, COMM_DEL_ITEM, COMM_QUERY};
 
 typedef struct command_t {
 	char comm_name[BUF_SIZE];
 	char arg1[BUF_SIZE];
 	char arg2[BUF_SIZE];
 } Command;
+
+Command *Command_new() {
+	Command *c = malloc(sizeof(Command));
+	c->arg1[0] = '\0';
+	c->arg2[0] = '\0';
+	c->comm_name[0] = '\0';
+	return c;
+}
+
+bool Command_is_valid_name(char *str) {
+	for (int i = 0; i < COMM_SIZE; ++i) {
+		if (strcmp(COMMAND_NAMES[i], str) == 0)
+			return true;
+	}
+	return false;
+}
+
+bool Command_is_valid_arg(char *str) {
+	int num = atoi(str);
+	if (num == 0 && !strcmp(str, "0") && !strcmp(str, "+0") && !strcmp(str, "-0")) {
+		return false;
+	}
+	return true;
+}
+
+bool Command_is_single_arg(char *name) {
+	return strcmp(name, COMM_QUERY) == 0;
+}
 
 
 typedef struct parser_t {
@@ -45,42 +75,86 @@ bool Parser_read(Parser *self) {
 
 Parser *Parser_new() {
 	Parser *p = malloc(sizeof(Parser));
+	p->has_next = true;
 	Parser_read(p);
 	return p;
 }
 
-void Parser_read_word(Parser *self, char *buff, char terminator) {
+bool Parser_read_word(Parser *self, char *buff, char terminator) {
 	int i = 0;
-	while (self->buff != terminator && self->has_next) {
+	while (self->buff != terminator && self->buff != '\n' && self->has_next) {
 		buff[i++] = self->buff;
 		Parser_read(self);
 	}
+	buff[i] = '\0';
+	return self->buff != '\n';
 }
 
 
 void Parser_skip_line(Parser *self) {
-	while (self->has_next && self->buff != '/n')
+	while (self->has_next && self->buff != '\n')
 		Parser_read(self);
+	Parser_read(self); //Read the first char of the next line
 }
 
-bool Parser_handle_next_command(Parser *self, Command *com) {
 
-	while (self->has_next && self->buff == '\n')
-		Parser_read(self);
+bool Parser_read_and_validate(Parser *self, Command *com) {
+	//Check if command is not interrupted by a new line
+	if (!Parser_read_word(self, com->comm_name, ' ')
+	    || !Parser_read_word(self, com->arg1, ' ')
+	    || !(Command_is_single_arg(com->comm_name) || Parser_read_word(self, com->arg2, '\n'))) {
+		return false;
+	}
 
-	if (self->buff == '#') {
+	//Check if all words together form a valid command syntax
+	if (!Command_is_valid_name(com->comm_name) || !Command_is_valid_arg(com->arg1) ||
+	    (!Command_is_valid_arg(com->comm_name) && !Command_is_valid_arg(com->arg2))) {
+		return false;
+	}
+
+	return true;
+}
+
+void print_execution_status(bool ok) {
+	printf(ok ? "OK\n", "ERROR\n");
+}
+
+void Parser_handle_next_line(Parser *self, struct Tree *t, Command *com) {
+
+	if (self->buff == '#' || self->buff == '\n') { //Ignored lines
 		Parser_skip_line(self);
-	} else {
-		
+		return;
+	}
 
+	if (!Parser_read_and_validate(self, com)) {
+		printf("ERROR\n");
+		return;
+	}
+
+	if (strcmp(com->comm_name, COMM_ADD_NODE) == 0) {
+		print_execution_status(Tree_add_node(t, atoi(com->arg1), atoi(com->arg2)));
+	} else if (strcmp(com->comm_name, COMM_ADD_ITEM) == 0) {
+		print_execution_status(Tree_add_item(t, atoi(com->arg1), atoi(com->arg2)));
+	} else if (strcmp(com->comm_name, COMM_DEL_ITEM) == 0) {
+		print_execution_status(Tree_remove_item(t, atoi(com->arg1), atoi(com->arg2));
+	} else if (strcmp(com->comm_name, COMM_DEL_USER) == 0) {
+		print_execution_status(Tree_remove_node(t, atoi(com->arg1)));
+	} else if (strcmp(com->comm_name, COMM_QUERY) == 0) {
+
+	} else {
+		printf("Unhandled command name %s", com->comm_name);
+		exit(1);
 	}
 
 }
 
 
 int main() {
+	struct Tree *t = Tree_new();
+	Command *buf_command = Command_new();
 	Parser *p = Parser_new();
 	while (p->has_next) {
-
+		Parser_handle_next_line(p, t, buf_command);
 	}
+	return 0;
 }
--- tree.h
+++ tree.h
@@ -15,4 +15,8 @@ bool Tree_add_node(struct Tree *self, int parent_id, int id);
 
 bool Tree_remove_node(struct Tree *self, int node);
 
+bool Tree_add_item(struct Tree *self, int node_id, int item);
+
+bool Tree_remove_item(struct Tree *self, int node_id, int item);
+
 #endif

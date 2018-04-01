fcd81b7ef3bb65160e62d5d237f4e67f6e9c4771
Samvel Abrahamyan <samvel1024@gmail.com>
2018-04-01 19:18:05 +0200

    Add testing file

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+prog=$1
+dir=$2
+
+if [ ! -f "$prog" ]; then
+    echo "Executable file not found!"
+    exit 1
+fi
+
+if [ ! -d "$dir" ]; then
+  echo "Could not find test directory!"
+  exit 1
+fi
+
+cd ${dir}
+
+for f in ./*.in; do
+  test=${f%.*}
+  expect_stderr="${test}.err"
+  expect_stdout="${test}.out"
+  printf "Running ${test} "
+  cat ${f} | ../${prog} 2> ../_temp_stderr 1> ../_temp_stdout
+  diff "../_temp_stderr" ${expect_stderr}
+  diff "../_temp_stdout" ${expect_stdout}
+  echo "OK"
+done
